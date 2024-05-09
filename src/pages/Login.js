3a53e9c9b15c1d9f4a3889058b9f5410aade0b4e
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-05-09 13:52:38 +0530

    login and register pages done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,82 +1,89 @@
-import React, { useState } from "react";
-import LoginImage from "../assets/gum-login.jpg";
-import { Link, useNavigate } from "react-router-dom";
-import UserService from "../services/UserService";
+import React, { useState } from 'react';
+import LoginImage from '../assets/login2.jpg';
+import google from '../assets/google.jpg';
+import { Link, useNavigate } from 'react-router-dom';
+import UserService from '../services/UserService';
+import '../css/login.css'
 
 const Login = () => {
-  const handleGoogleLogin = async () => {
-    window.location.href = "http://localhost:8080/oauth2/authorization/google";
-  };
-
   const [user, setUser] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setUser({ ...user, [e.target.name]: value });
-  };
-  const loginUser = async (e) => {
-    console.log(user);
 
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const loginUser = async (e) => {
     e.preventDefault();
     UserService.loginUser(user)
       .then((response) => {
         console.log(response);
-        navigate("/home");
-        alert("Welcom!");
-        localStorage.setItem("user", JSON.stringify(response.data));
+        navigate('/home');
+        alert('Welcome!');
+        localStorage.setItem('user', JSON.stringify(response.data));
       })
       .catch((error) => {
         console.log(error);
         alert(error?.response?.data);
       });
   };
+
+  const handleGoogleLogin = async () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  };
+
   return (
-    <div className=" flex flex-row">
-      <div className="w-3/5">
-        <img className="h-screen " src={LoginImage} alt="Login Image" />
-      </div>
-      <div className="w-2/5">
-        <div className="bg-black bg-opacity-40 absolute inset-0 "></div>
-        <div className="bg-white h-full shadow-md rounded-md space-y-10 p-8 w-100 relative z-10 flex flex-col items-center justify-center">
-          <h2 className="text-xl font-semibold mb-4">Login</h2>
+    <div
+      className="flex items-center justify-center h-screen"
+      style={{ backgroundImage: `url(${LoginImage})`, backgroundSize: 'cover' }}
+    >
+      <div
+        className="bg-white bg-opacity-90 backdrop-filter backdrop-blur-lg h-auto p-20 rounded-lg shadow-lg"
+        style={{ minWidth: '400px', minHeight: '700px' }}
+      >
+        <h2 className="text-xl font-semibold mb-5 text-center bebas-neue-regular">FIT TRACK</h2>
+        <h2 className="text-xl font-semibold mt-[50px] mb-4 text-center ">Login</h2>
+        <form className="space-y-10">
           <input
             type="text"
             name="email"
-            onChange={(e) => handleChange(e)}
+            onChange={handleChange}
             placeholder="Username"
-            className="w-full px-3 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
+            className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
           />
           <input
             type="password"
             name="password"
-            onChange={(e) => handleChange(e)}
+            onChange={handleChange}
             placeholder="Password"
-            className="w-full px-3 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
+            className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
           />
           <button
+            type="submit"
             onClick={loginUser}
-            className="w-full bg-blue-500 mb-5 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
           >
             Login
           </button>
-
           <button
+            type="button"
             onClick={handleGoogleLogin}
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300 flex justify-center items-center"
           >
-            google login
+            <img src={google} alt="Google logo" className="mr-2" style={{ height: '20px' }} />
+            Login With
           </button>
-          <div className="flex flex-row">
-            <p className="mr-1">Don't have an Account?</p>
-            <Link to="/register">
-              <p className="ml-1 text-blue-600 underline cursor-pointer">
-                Register now!
-              </p>
-            </Link>
-          </div>
+        </form>
+        <div className="flex flex-row justify-center mt-4">
+          <p className="mr-1">Don't have an Account?</p>
+          <Link to="/register">
+            <p className="text-blue-600 underline cursor-pointer">Register now!</p>
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,244 +1,183 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import RegisterImage from "../assets/gym-register.jpg";
-import UserService from "../services/UserService";
-import { app, imageDb } from "../config/Config";
-import { v4 } from "uuid";
-import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import UserService from '../services/UserService';
+import { app } from '../config/Config';
+import { v4 } from 'uuid';
+import { ref, uploadBytes, getDownloadURL, getStorage } from 'firebase/storage';
 
 const storage = getStorage(app);
 
 const Register = () => {
-  const [user, setUser] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    username: "",
-    password: "",
-    phoneNumber: "",
-    profilePictureUrl: "",
-    bio: "",
-  });
-  const navigate = useNavigate();
+    const [user, setUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+        phoneNumber: '',
+        profilePictureUrl: '',
+        bio: '',
+    });
+    const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setUser({ ...user, [e.target.name]: value });
-  };
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setUser({ ...user, [e.target.name]: value });
+    };
 
-  //upload image
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [uploadedUrls, setUploadedUrls] = useState([]);
+    const [previewUrl, setPreviewUrl] = useState(null);
+    const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
 
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [uploadedUrls, setUploadedUrls] = useState([]);
-  const [previewUrl, setPreviewUrl] = useState(null);
-  const [errors, setErrors] = useState({});
-  const [loading, setLoading] = useState(null);
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedFile(file);
+        setPreviewUrl(URL.createObjectURL(file));
+        setErrors({ ...errors, profilePictureUrl: '' });
+    };
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setSelectedFile(file);
-    setPreviewUrl(URL.createObjectURL(file));
-    // Clear errors when user selects a file
-    setErrors({ ...errors, profilePictureUrl: "" });
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const formErrors = {};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true); // Set loading to true when submitting form
-    const formErrors = {};
+        if (!selectedFile) {
+            formErrors.profilePictureUrl = 'Profile Picture is required';
+        }
 
-    // Basic validation for profile picture
-    if (!selectedFile) {
-      formErrors.profilePictureUrl = "Profile Picture is required";
-    }
+        if (!user.firstName.trim()) {
+            formErrors.firstName = 'First Name is required';
+        }
+        if (!user.lastName.trim()) {
+            formErrors.lastName = 'Last Name is required';
+        }
+        if (!user.email.trim()) {
+            formErrors.email = 'Email is required';
+        } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+            formErrors.email = 'Email address is invalid';
+        }
 
-    // Basic validation for other fields
-    if (!user.firstName.trim()) {
-      formErrors.firstName = "First Name is required";
-    }
-    if (!user.lastName.trim()) {
-      formErrors.lastName = "Last Name is required";
-    }
-    if (!user.email.trim()) {
-      formErrors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
-      formErrors.email = "Email address is invalid";
-    }
-    // Add more validations as needed
+        setErrors(formErrors);
 
-    setErrors(formErrors);
+        if (Object.keys(formErrors).length === 0) {
+            let imageUrl = null;
 
-    // If no errors, submit form
-    if (Object.keys(formErrors).length === 0) {
-      let imageUrl = null;
+            if (selectedFile) {
+                const imageRef = ref(storage, `/images/${selectedFile.name}`);
+                await uploadBytes(imageRef, selectedFile);
+                imageUrl = await getDownloadURL(imageRef);
+            }
 
-      if (selectedFile) {
-        const imageRef = ref(storage, `/images/${selectedFile.name}`);
-        await uploadBytes(imageRef, selectedFile);
-        imageUrl = await getDownloadURL(imageRef);
-      }
+            const data = {
+                ...user,
+                profilePictureUrl: imageUrl,
+            };
 
-      const data = {
-        ...user,
-        profilePictureUrl: imageUrl,
-      };
+            UserService.saveUser(data)
+                .then((response) => {
+                    console.log(response);
+                    navigate('/');
+                    alert('User Registered!');
+                })
+                .catch((error) => {
+                    console.log('error');
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        } else {
+            setLoading(false);
+        }
+    };
 
-      UserService.saveUser(data)
-        .then((response) => {
-          console.log(response);
-          navigate("/");
-          alert("User Registered!");
-        })
-        .catch((error) => {
-          console.log("error");
-        })
-        .finally(() => {
-          setLoading(false); // Set loading to false when registration process is completed
-        });
-    } else {
-      setLoading(false); // Set loading to false if there are form errors
-    }
-  };
-
-  return (
-    <div>
-      <div className="flex flex-row ">
-        <div className="w-2/5">
-          <div className="bg-black  bg-opacity-40 absolute inset-0 "></div>
-          <div className="">
-            <div className="bg-white  h-screen shadow-md rounded-md p-8 w-100 relative z-10 ">
-              <h2 className="text-xl font-semibold mb-4">Register</h2>
-              <form className=" space-y-10" onSubmit={handleSubmit}>
-                <div className=" flex flex-col ">
-                  <div className=" m-auto flex">
-                    {previewUrl && (
-                      <img
-                        src={previewUrl}
-                        className=" rounded-full w-[100px] h-[100px] "
-                        alt="Selected"
-                        
-                      />
-                    )}
-                  </div>
-                  <label>Profile Picture</label>
-                  <input type="file" onChange={handleFileChange} />
-                </div>
-                <div className="flex flex-row gap-3">
-                  <label htmlFor="firstname" className="block text-gray-700">
-                    First Name
-                    <input
-                      type="text"
-                      name="firstName"
-                      // value={user.firstName}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="First Name"
-                      className="w-full px-3 mt-2 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
-                    />
-                  </label>
-                  <label
-                    htmlFor="lastName"
-                    className="block text-gray-700 ml-auto"
-                  >
-                    Last Name
-                    <input
-                      type="text"
-                      name="lastName"
-                      // value={user.lastName}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="Last Name"
-                      className="w-full px-3 mt-2 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
-                    />
-                  </label>
-                </div>
-                <div className="">
-                  <label htmlFor="email" className="block text-gray-700">
-                    Email
-                    <input
-                      type="text"
-                      name="email"
-                      // value={user.email}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="Email"
-                      className="w-full mt-2 px-3 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
-                    />
-                  </label>
-                </div>
-                <div className="">
-                  <label htmlFor="phoneNumber" className="block text-gray-700">
-                    Phone Number
-                    <input
-                      type="text"
-                      name="phoneNumber"
-                      // value={user.phoneNumber}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="Phone Number"
-                      className="w-full px-3 mt-2 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
-                    />
-                  </label>
-                </div>
-                <div className="flex flex-row gap-3">
-                  <label htmlFor="username" className="block text-gray-700">
-                    User Name
-                    <input
-                      type="text"
-                      name="username"
-                      // value={user.username}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="User Name"
-                      className="w-full px-3 mt-2 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
-                    />
-                  </label>
-                  <label
-                    htmlFor="password"
-                    className="block text-gray-700 ml-auto"
-                  >
-                    Password
-                    <input
-                      type="password"
-                      name="password"
-                      // value={user.username}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="Password"
-                      className="w-full px-3 mt-2 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
-                    />
-                  </label>
-                </div>
-                <div>
-                  <label htmlFor="address" className="block text-gray-700">
-                    Address
+    return (
+        <div className="flex items-center justify-center h-screen bg-[#ef4444]-100">
+            <div className="bg-white shadow-md rounded-md p-10 w-96"
+            style={{ minWidth: '800px', minHeight: '700px' }}>
+                <h2 className="text-xl font-semibold mb-4">Register</h2>
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    
+                    <div className="space-y-3">
+                        <input
+                            type="text"
+                            name="firstName"
+                            onChange={handleChange}
+                            placeholder="First Name"
+                            className="block w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+                        />
+                        <input
+                            type="text"
+                            name="lastName"
+                            onChange={handleChange}
+                            placeholder="Last Name"
+                            className="block w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+                        />
+                         <input
+                            type="text"
+                            name="username"
+                            onChange={handleChange}
+                            placeholder="User Name"
+                            className="block w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            placeholder="Password"
+                            className="block w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+                        />
+                        <input
+                            type="text"
+                            name="email"
+                            onChange={handleChange}
+                            placeholder="Email"
+                            className="block w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+                        />
+                        <input
+                            type="text"
+                            name="phoneNumber"
+                            onChange={handleChange}
+                            placeholder="Phone Number"
+                            className="block w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+                        />  
+                    </div>
                     <textarea
-                      name="bio"
-                      // value={user.address}
-                      onChange={(e) => handleChange(e)}
-                      placeholder="Address"
-                      className="w-full mt-2 px-3 py-2 rounded-md border border-gray-300 mb-3 focus:outline-none focus:border-blue-500 bg-white bg-opacity-10"
+                        name="bio"
+                        onChange={handleChange}
+                        placeholder="Address"
+                        className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
                     ></textarea>
-                  </label>
+                    <div className="flex flex-col items-center">
+                        {previewUrl && (
+                            <img
+                                src={previewUrl}
+                                className="rounded-full w-24 h-24 mb-3"
+                                alt="Selected"
+                            />
+                        )}
+                        <label>Profile Picture</label>
+                        <input type="file" onChange={handleFileChange} />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+                    >
+                        Register
+                    </button>
+                </form>
+                <div className="flex justify-center mt-5">
+                    <p>Already have an account?</p>
+                    <Link to="/login">
+                        <p className="ml-1 text-blue-600 underline cursor-pointer">
+                            Login
+                        </p>
+                    </Link>
                 </div>
-                <button
-                  type="submit"
-                  className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
-                >
-                  Register
-                </button>
-              </form>
-              <div className="flex flex-row mt-5">
-                <p className="mr-1">Already have an account?</p>
-                <Link to="/login">
-                  <p className="ml-1 text-blue-600 underline cursor-pointer">
-                    Login
-                  </p>
-                </Link>
-              </div>
             </div>
-          </div>
         </div>
-        <div className="">
-          <img className=" h-screen " src={RegisterImage} alt="Login Image" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Register;
