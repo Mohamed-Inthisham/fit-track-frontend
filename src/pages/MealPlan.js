898e9f7e41275dc9405c1c934e07510953c9480a
IT21356704 <it21356704@my.sliit.lk>
2024-05-10 13:25:17 +0530

    workout and mealplan completed

--- src/pages/MealPlan.js
+++ src/pages/MealPlan.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SideBar } from "./SideBar";
 
-import BgImage from "../assets/top-view-table-full-delicious-food-composition.jpg";
+import BgImage from "../assets/food6.jpeg";
 import MealPlanService from "../services/MealPlanService";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { app } from "../config/Config";
