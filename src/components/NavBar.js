a208398f607e1c60d17b773ef1161a855393373b
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-05-10 11:57:56 +0530

    sidebar and navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, Typography } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
-import '../css/Navbar.css'
+import '../css/Navbar.css';
 
 export function NavBar({ user }) {
   const [loggedIn, setLoggedIn] = useState({});
@@ -31,12 +31,11 @@ export function NavBar({ user }) {
           Find Friends
         </Link>
       </Typography>
-      
     </ul>
   );
 
   return (
-    <div className=" top-0 z-50 fixed max-h-full w-full">
+    <div className="top-0 z-50 fixed max-h-full w-full">
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-[#B80000]">
         <div className="flex items-center justify-between text-white">
           <Link to="/home">
@@ -44,32 +43,29 @@ export function NavBar({ user }) {
               Fit Track
             </Typography>
           </Link>
+          
           <div className="flex items-center gap-4 text-white">
             <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1 ">
+            <Link to="/profile" className="flex items-center gap-x-1">
               {loggedIn.username}
               {loggedIn.provider === "google" ? (
                 <img
-                  className=" w-[40px] h-[40px] rounded-full "
+                  className="w-[40px] h-[40px] rounded-full"
                   src={loggedIn.imageUrl}
+                  alt="User's profile"
                 />
               ) : (
                 <img
-                  className=" w-[40px] h-[40px] rounded-full "
+                  className="w-[40px] h-[40px] rounded-full"
                   src={loggedIn.profilePictureUrl}
+                  alt="User's profile"
                 />
               )}
-              <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <button className="flex items-center text-white ml-5" onClick={handleLogOut} >
-          LogOut
-        </button>
-      </Typography>
-            </div>
+            </Link>
+            
+            <button className="flex items-center text-white ml-5" onClick={handleLogOut}>
+              LogOut
+            </button>
           </div>
         </div>
       </Navbar>
